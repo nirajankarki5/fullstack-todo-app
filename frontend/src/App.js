72c2c035002b517b45948ff32f7919fbcbec3bcc
nirajankarki5 <nirajankarki5@gmail.com>
2023-09-08 19:58:23 -0400

    added input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 function App() {
   return (
     <div className="App">
-      <h1> Todo</h1>
+      <header>
+        <nav className="navbar">
+          <h2>Add your todo</h2>
+        </nav>
+      </header>
+      <main className="main-container">
+        <form>
+          <div className="input-div">
+            <input type="text" placeholder="title..." required />
+            <input type="date" required />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </main>
     </div>
   );
 }

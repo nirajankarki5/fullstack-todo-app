bd0cbc1f4040040ae660245c406662c6883e12dd
nirajankarki5 <nirajankarki5@gmail.com>
2023-09-10 14:50:06 -0400

    enable cors requests

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const todoRouter = require("./routes/todo_route");
+
 const app = express();
 
-const todoRouter = require("./routes/todo_route");
+// Enable all CORS requests
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
